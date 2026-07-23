2ad9fb632478b3de6030191b425b6e51810af759
sabbirRashed <srsabbirrahman12@gmail.com>
2026-07-23 23:58:57 +0600

    added handle login function

--- src/app/(authentication)/login/page.jsx
+++ src/app/(authentication)/login/page.jsx
@@ -6,6 +6,14 @@ import { FcGoogle } from "react-icons/fc";
 
 
 const LoginPage = () => {
+
+    const handleLogin = (e)=>{
+        e.preventDefault();
+
+        const formData = new FormData(e.currentTarget);
+        const loginData = Object.fromEntries(formData.entries());
+        console.log(loginData);
+    }
     return (
         <div className='min-h-screen w-11/12 max-w-7xl mx-auto py-30'>
             <h2 className='text-center text-2xl md:text-3xl font-semibold tracking-wide'>Welcome back <br /> to your Nest</h2>
@@ -13,7 +21,7 @@ const LoginPage = () => {
             <div className="border border-(--color-primary)! shadow-2xl! shadow-amber-600! rounded-2xl max-w-md mx-auto p-10 mt-6 space-y-4">
 
                 <Form
-                    // onSubmit={onSubmit}
+                    onSubmit={handleLogin}
                     className="flex w-full flex-col gap-6" >
 
                     <TextField
