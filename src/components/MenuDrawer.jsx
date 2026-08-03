f106fbd0422f74cc78cc1c49dcb1df5a6cb69d74
sabbirRashed <srsabbirrahman12@gmail.com>
2026-08-03 22:10:04 +0600

    added logout , login and signup button on drawer

--- src/components/MenuDrawer.jsx
+++ src/components/MenuDrawer.jsx
@@ -5,9 +5,10 @@ import { useTheme } from 'next-themes';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import toast from 'react-hot-toast';
 import { FiMoon } from 'react-icons/fi';
 import { IoMdTrendingUp } from 'react-icons/io';
-import { IoHomeOutline, IoSunnyOutline } from 'react-icons/io5';
+import { IoHomeOutline, IoLogOutOutline, IoSunnyOutline } from 'react-icons/io5';
 import { TbActivity, TbBulb, TbHome, TbNotebook, TbSquareRoundedPlus } from 'react-icons/tb';
 
 const MenuDrawer = () => {
@@ -19,6 +20,17 @@ const MenuDrawer = () => {
 
     const { data, isPending } = authClient.useSession();
     const user = data?.user;
+
+    const handleLogout = async () => {
+        const { data, error } = await authClient.signOut()
+
+        if (data.success) {
+            toast.success('Logout successfully')
+        }
+        if (error) {
+            toast.error(error.message)
+        }
+    }
 
     return (
         <Drawer>
@@ -40,18 +52,20 @@ const MenuDrawer = () => {
                                 <p className="font-bold text-lg text-(--color-secondary)"><span className="text-(--color-primary)">Spark</span>Nest</p>
                             </div>
                             <Separator className=' border-b border-(--color-border)' />
-                            <div className='flex items-center gap-2.5 px-5 py-2'>
-                                <Avatar size="" >
-                                    <Avatar.Image
-                                        src={user?.image}
-                                        alt={user?.name} />
-                                    <Avatar.Fallback className='text-xs'>{user?.name && user?.name[0]}</Avatar.Fallback>
-                                </Avatar>
-                                <div>
-                                    <p className='text-sm font-medium'>{user?.name}</p>
-                                    <Link href={'/profile'}><span className='text-xs text-(--color-secondary)'>View Profile</span></Link>
+                            {
+                                user && <div className='flex items-center gap-2.5 px-5 py-2'>
+                                    <Avatar size="" >
+                                        <Avatar.Image
+                                            src={user?.image}
+                                            alt={user?.name} />
+                                        <Avatar.Fallback className="bg-(--color-primary)/10 text-(--color-primary) text-sm font-medium">{user?.name && user?.name[0] || "U"}</Avatar.Fallback>
+                                    </Avatar>
+                                    <div>
+                                        <p className='text-sm font-medium'>{user?.name}</p>
+                                        <Link href={'/profile'}><span className='text-xs text-(--color-secondary)'>View Profile</span></Link>
+                                    </div>
                                 </div>
-                            </div>
+                            }
                             <Separator className=' border-b border-(--color-border)' />
                         </Drawer.Header>
                         <Drawer.Body >
@@ -78,20 +92,53 @@ const MenuDrawer = () => {
                                         isIconOnly
                                         size='sm'
                                         variant='light'
-                                        onClick={()=> setTheme("light")}
-                                        className={` ${isDark? " text-(--color-text)/20": "bg-gray-200"}`}>
+                                        onClick={() => setTheme("light")}
+                                        className={` ${isDark ? " text-(--color-text)/20" : "bg-gray-200"}`}>
                                         <IoSunnyOutline className=" h-5 w-5 cursor-pointer" />
                                     </Button>
 
                                     <Button
                                         isIconOnly size='sm'
                                         variant='light'
-                                        onClick={()=>setTheme("dark")}
-                                        className={`${isDark? "bg-white/10": "text-(--color-text)/20"}`}>
+                                        onClick={() => setTheme("dark")}
+                                        className={`${isDark ? "bg-white/10" : "text-(--color-text)/20"}`}>
                                         <FiMoon className=" h-5 w-5 cursor-pointer" />
                                     </Button>
 
                                 </div>
+                            </div>
+
+                            {/* authentication */}
+                            <div className='px-5 py-4'>
+                                {
+                                    user ? <>
+                                        <Button
+                                            variant='outline'
+                                            className={' hover:bg-(--color-danger)/90 text-danger hover:text-white transition-colors duration-200 w-full rounded-md py-5 '}
+                                            onPress={handleLogout}
+                                        >
+                                            <IoLogOutOutline />
+                                            Log out
+                                        </Button>
+                                    </> : <>
+                                        <Link href={'/login'}>
+                                            <Button
+                                                size="sm"
+                                                className={'rounded-md w-full py-5 btn-secondary transition-colors duration-300'}
+                                            >
+                                                Log in
+                                            </Button>
+                                        </Link>
+                                        <Link href={'/register'}>
+                                            <Button
+                                                size="sm"
+                                                className={'rounded-md w-full py-5 btn-primary transition-colors duration-300 mt-2'}
+                                            >
+                                                Register
+                                            </Button>
+                                        </Link>
+                                    </>
+                                }
                             </div>
                         </Drawer.Body>
 
